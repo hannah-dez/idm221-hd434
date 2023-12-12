86a852e24ec4339ecaa895ba66a281b99e96fceb
hannah-dez <hd434@drexel.edu>
2023-12-12 13:41:55 -0500

    css updates and gitignore update

--- carousels.js
+++ carousels.js
@@ -24,39 +24,11 @@ function prevAboutSlide() {
   showAboutSlide(aboutCarouselIndex - 1);
 }
 
-// TALL CAROUSEL
-const tallCarousel = document.querySelector('.tall-carousel');
-const tallCarouselItems = document.querySelectorAll('.tall-carousel-item');
-let tallCarouselIndex = 0;
-
-function showTallSlide(index) {
-  if (index < 0) {
-    tallCarouselIndex = tallCarouselItems.length - 1;
-  } else if (index >= tallCarouselItems.length) {
-    tallCarouselIndex = 0;
-  } else {
-    tallCarouselIndex = index;
-  }
-
-  const translateValue = -tallCarouselIndex * 100 + 'vh';
-  tallCarousel.style.transform = `translateY(${translateValue})`;
-}
-
-function nextTallSlide() {
-  showTallSlide(tallCarouselIndex + 1);
-}
-
-function prevTallSlide() {
-  showTallSlide(tallCarouselIndex - 1);
-}
-
 // Attach event listeners to the buttons
 const prevAboutButton = document.querySelector('.carousel-button.prev.about');
 const nextAboutButton = document.querySelector('.carousel-button.next.about');
-const prevTallButton = document.querySelector('.carousel-button.prev.tall');
-const nextTallButton = document.querySelector('.carousel-button.next.tall');
+
 
 prevAboutButton.addEventListener('click', prevAboutSlide);
 nextAboutButton.addEventListener('click', nextAboutSlide);
-prevTallButton.addEventListener('click', prevTallSlide);
-nextTallButton.addEventListener('click', nextTallSlide);
+
